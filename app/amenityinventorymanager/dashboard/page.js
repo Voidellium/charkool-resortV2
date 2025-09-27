43c8fe3e17513069d01ade06b54064baa4c6923f
Alixer <voidellum@gmail.com>
2025-09-27 23:21:43 +0800

    fixed that nigger

---
+++ app/amenityinventorymanager/dashboard/page.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Amenity Inventory Manager Dashboard</h1>
+      <p>This page is no longer in use.</p>
+    </div>
+  );
+}
